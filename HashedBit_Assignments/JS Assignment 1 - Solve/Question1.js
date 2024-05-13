b8b5d37148534d8ac0e2988013a4df7c40275b48
nancykumari9c <167325123+nancykumari9c@users.noreply.github.com>
2024-05-13 21:41:42 +0530

    Add files via upload

---
+++ HashedBit_Assignments/JS Assignment 1 - Solve/Question1.js
@@ -0,0 +1,11 @@
+//1. What is the output of this snippet of code?
+
+function foo() {
+    x = 1;
+    function x() { }
+    var x = '2';
+    return x;
+}
+alert( typeof foo() );
+
+//Output:- String
---
+++ HashedBit_Assignments/JS Assignment 1 - Solve/Question2.js
@@ -0,0 +1,13 @@
+//2. What is the output of this snippet of code?
+
+function foo() {
+    return x;
+    x = 1;
+    
+    function x() { }
+    
+    var x = '2';
+}
+console.log( typeof foo() );   
+
+//Output:- Function
